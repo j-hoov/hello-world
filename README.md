dd47614d92597daad24d99e75b984585936f037f
Juliana Hoover <jgh23@duke.edu>
2019-01-24 09:46:34 -0500

    add readme and index

---
+++ README.md
@@ -0,0 +1,10 @@
+# This is my first sketch!
+------
+
+### JitterBug
+
+![Jitter](images/jitter1.png?raw=true "Jitter")
+![Jitter](images/jitter2.png?raw=true "Jitter")
+
+This sketch is inspired by PacMan and the "tickle" demo from the p5.js web editor. The first goal of the sketch was to explore random jitter motion within a bounded canvas. The second was to pull from the PacMan game environment to evoke a well-known "chase experience". Further extending this piece would incorporate PacMan sound effects and other objects for JitterBug to chase on the gray screen.   
+
--- sketch.js
+++ sketch.js
@@ -1,65 +1,88 @@
 
-let font;
-    fontsize = 80;
 
-function preload(){
-    font = loadFont('assets/Angelow.otf');
+var message1 = "jitter",
+    font,
+    bounds, // holds x, y, w, h of the text's bounding box
+    fontsize = 50,
+    x, y; // x and y coordinates of the text
+
+var song;
+
+let img;
+
+function preload() {
+    font0 = loadFont('assets/Angelow.otf');
+    font = loadFont('assets/PAC-FONT.TTF');
+    img = loadImage('images/pacmanboard.png');
+    ghostt = loadImage('images/ghostt.png');
+    ghosty = loadImage('images/ghosty.png');
+    ghostr = loadImage('images/ghostr.png');
+    song = loadSong('sounds/pacman.mp3');
 }
 
-function setup(){
-    createCanvas(900, 400);
-    background(160);
+function setup() {
+    createCanvas(500, 500);
+
+    // set up the font
     textFont(font);
     textSize(fontsize);
-    textAlign(CENTER,CENTER);
+
+    // get the width and height of the text so we can center it initially
+    bounds = font.textBounds(message1, 0, 0, fontsize);
+    x = width / 2 - bounds.w / 2;
+    y = height / 2 - bounds.h / 2;
+    c = random(-200,200);
+    d = random(-200,200);
+    j = 1.5
+    k = .5;
+    }
+
+function draw() {
+background(204, 120);
+
+// check if the mouse is inside the bounding box and tickle if so
+if ( mouseX >= bounds.x && mouseX <= bounds.x + bounds.w &&
+    mouseY >= bounds.y && mouseY <= bounds.y + bounds.h) {
+    //song.play();
+    a = random(-5,5);
+    b = random(-5,5);
+    background(270);
+    image(img,0,-35,500,535);
+    if (mouseX < 150){
+        image(ghostr,mouseX+a,mouseY+b-80,70,70);
+    }
+    if (mouseX >= 150 && mouseX <= 275){
+        image(ghosty,mouseX+a+80,mouseY+b,70,70);
+    }
+    if (mouseX > 275 && mouseX <= 500){
+        image(ghostt,mouseX+a-120,mouseY+b-40,70,70);
+    }
+    fill(255);
+    x += random(-10, 10);
+    y += random(-15, 15);
+    }
+else {
+    //song.stop();
+    fill(215);
+    x += j;
+    y += k;
 }
 
-x1 = 15; 
-x2 = 885;
-f = 25;
-
-function draw(){
-
-// small gradient circles
-    fill(f);
-    ellipse(x1,40,20,20); 
-
-    fill(f);
-    ellipse(x2,360,20,20);
-    
-// central circles
-    fill(210);
-    ellipse(265,200,300,300);
-
-    fill(140);
-    ellipse(555,200,170,170);
-
-    fill(80);
-    ellipse(730,200,80,80);
- 
-// text
-   textAlign(RIGHT);
-   fill(65);
-   drawWords(width * 0.25);
-   
-   textAlign(CENTER);
-   fill(130);
-   drawWords(width * 0.5);
-
-   textAlign(LEFT);
-   fill(220);
-   drawWords(width * 0.75);
-
-    x1 = x1+25;
-    x2 = x2-25;
-    f = f+10;
+if (bounds.x + bounds.w >= width - 5){
+    j = -1.5;
+}
+if (bounds.x <= width - 495){
+    j = 1.5;
+}
+if (bounds.y + bounds.h <= height - 430){
+    k = .5;
+}
+if (bounds.y >= height - 70){
+    k = -.5;
 }
 
-function drawWords(x){
-    
-    text('hello',x,150);
+// write the text in black and get its bounding box
+text(message1, x+c, y+d);
+bounds = font.textBounds(message1,x+c,y+d,fontsize);
 
-    text('world',x,220);
 }
-
-
